3761566a36b18c4a29642b51877a5211b4f21052
Rittik Kumar <rkrittik17@gmail.com>
2021-01-06 19:38:14 +0530

    created DisplayComponent and BtnComponent

---
+++ src/Components/BtnComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function BtnComponent() {
+  return (
+    <div>
+      <h1>btn</h1>
+    </div>
+  );
+}
+
+export default BtnComponent;
---
+++ src/Components/DisplayComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function DisplayComponent() {
+  return (
+    <div >
+      <h1>DisplayComponent</h1>
+    </div>
+  );
+}
+
+export default DisplayComponent;
