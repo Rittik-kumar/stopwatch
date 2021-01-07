caf5d6867be0e8fdff964553b97279537652e6a5
Rittik Kumar <rkrittik17@gmail.com>
2021-01-07 19:55:03 +0530

    created stop, start, resume, reset buttons

--- src/Components/BtnComponent.js
+++ src/Components/BtnComponent.js
@@ -1,8 +1,29 @@
 import React from 'react';
-function BtnComponent() {
+function BtnComponent(props) {
   return (
     <div>
-      <h1>btn</h1>
+      {(props.status === 0) ? 
+        <button className="stopwatch-btn stopwatch-btn-gre" 
+        onClick={props.start}>Start</button> : ""
+      }
+      
+      {(props.status === 1) ? 
+       <div>
+         <button className="stopwatch-btn stopwatch-btn-red" 
+          onClick={props.stop}>Stop</button>
+         <button className="stopwatch-btn stopwatch-btn-yel" 
+          onClick={props.reset}>Reset</button>
+        </div> : ""
+      }
+
+      {(props.status === 2) ? 
+       <div>
+         <button className="stopwatch-btn stopwatch-btn-gre" 
+          onClick={props.resume}>Resume</button>
+         <button className="stopwatch-btn stopwatch-btn-yel" 
+          onClick={props.reset}>Reset</button>
+        </div> : ""
+      }
     </div>
   );
 }
