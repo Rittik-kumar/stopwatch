b5a29395c838a41e1a7260bdc1d1fda190345c89
Rittik Kumar <rkrittik17@gmail.com>
2021-01-07 18:56:46 +0530

    created time h,m,s,ms

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import DisplayComponent from './Components/DisplayComponent';
 import BtnComponent from './Components/BtnComponent';
 
 function App() {
+  const[time, setTime] = useState({ms:0, s:0, m:0, h:0});
   return (
     <div className="main-section">
       <div className="clock-holder">
         <div className="stopwatch">
-           <DisplayComponent />
+           <DisplayComponent time={time} />
            <BtnComponent />
         </div>
       </div>
--- src/Components/DisplayComponent.js
+++ src/Components/DisplayComponent.js
@@ -1,8 +1,11 @@
 import React from 'react';
-function DisplayComponent() {
+function DisplayComponent(props) {
   return (
     <div >
-      <h1>DisplayComponent</h1>
+     <span>{(props.time.h >=10) ? props.time.h : "0"+ props.time.h}</span>&nbsp;:&nbsp;
+     <span>{(props.time.m >=10) ? props.time.m : "0"+ props.time.m}</span>&nbsp;:&nbsp;
+     <span>{(props.time.s >=10) ? props.time.s : "0"+ props.time.s}</span>&nbsp;:&nbsp;
+     <span>{(props.time.ms >=10) ? props.time.ms : "0"+ props.time.ms}</span>&nbsp;:&nbsp;
     </div>
   );
 }
