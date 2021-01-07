7cac2da64da4cd81e2b243777d50643d9c1e3de2
Rittik Kumar <rkrittik17@gmail.com>
2021-01-07 19:55:44 +0530

    button functionality

--- src/App.js
+++ src/App.js
@@ -4,13 +4,54 @@ import DisplayComponent from './Components/DisplayComponent';
 import BtnComponent from './Components/BtnComponent';
 
 function App() {
-  const[time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const[time, setTime] = useState({ms:0, s:50, m:59, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run,10));
+  };
+
+  var updateMs = time.ms, updateS = time.s, updateM = time.m, updateH = time.h;
+
+  const run = () => {
+    if(updateM === 60){
+      updateH++;
+      updateM =0;
+    }
+    if(updateS === 60){
+      updateM++;
+      updateS =0;
+    }
+    if(updateMs === 60){
+      updateS++;
+      updateMs =0;
+    }
+    updateMs++;
+    return setTime({ms:updateMs, s:updateS, m:updateM, h:updateH});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0});
+  };
+
+  const resume = () => start();
+  
+
   return (
     <div className="main-section">
       <div className="clock-holder">
         <div className="stopwatch">
            <DisplayComponent time={time} />
-           <BtnComponent />
+           <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start} />
         </div>
       </div>
     </div>
